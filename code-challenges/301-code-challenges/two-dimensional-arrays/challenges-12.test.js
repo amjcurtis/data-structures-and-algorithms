2258a2fd4b909e1a7971350b22050b8eaed43e32
amjcurtis <curtis.andrewm@gmail.com>
2019-02-27 23:15:23 -0800

    Add second attempt at challenge 1

--- code-challenges/301-code-challenges/two-dimensional-arrays/challenges-12.test.js
+++ code-challenges/301-code-challenges/two-dimensional-arrays/challenges-12.test.js
@@ -14,26 +14,27 @@ Return the array of the total number of cookies sold per hour for all of the sto
 
 const hoursOpen = ['9 a.m.', '10 a.m.', '11 a.m.', '12 p.m.', '1 p.m.', '2 p.m.', '3 p.m.', '4 p.m.', '5 p.m.', '6 p.m.', '7 p.m.', '8 p.m.'];
 
-const firstPike = [17, 18, 23, 24, 24, 12, 13, 27, 30, 20, 24, 18];
-const seaTac = [26, 5, 5, 59, 23, 39, 38, 20, 30, 7, 59, 43];
+const firstPike =     [17, 18, 23, 24, 24, 12, 13, 27, 30, 20, 24, 18];
+const seaTac =        [26, 5, 5, 59, 23, 39, 38, 20, 30, 7, 59, 43];
 const seattleCenter = [7, 14, 19, 22, 15, 4, 23, 27, 28, 23, 1, 29];
-const capHill = [5, 85, 58, 51, 50, 13, 33, 32, 47, 94, 31, 62];
-const alkiBeach = [33, 31, 147, 130, 27, 93, 38, 126, 141, 63, 46, 17];
+const capHill =       [5, 85, 58, 51, 50, 13, 33, 32, 47, 94, 31, 62];
+const alkiBeach =     [33, 31, 147, 130, 27, 93, 38, 126, 141, 63, 46, 17];
 
 const cookieStores = [firstPike, seaTac, seattleCenter, capHill, alkiBeach];
 
 const grandTotal = (stores) => {
+
   let hourlyCookieTotals = [];
 
-  // i is store number?
-  // j is hour number?
-  
-  for (let i = 0; i <= stores.length-1; i++) {
-    // TODO Set counter to 0
-
-    for (let j = 0; j <= stores[i].length-1; j++) {
-      
+  for (let i = 0; i <= stores.length - 1; i++) {
+    var count = 0;
+    for (let j = 0; j <= stores[i].length - 1; j++) {
+      console.log('stores[j][i]', stores[j][i]);
+      count = stores[j][i] + count;
+      console.log('count', count);
     }
+    hourlyCookieTotals.push(count);
+    console.log('hourlyCookieTotals', hourlyCookieTotals);
   }
   return hourlyCookieTotals;
 }
