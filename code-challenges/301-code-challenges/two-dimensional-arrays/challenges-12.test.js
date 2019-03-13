ac5f099469a3c13f1ae8177df4a10309f624e343
amjcurtis <curtis.andrewm@gmail.com>
2019-03-12 23:58:35 -0700

    Add solution to challenge 2

--- code-challenges/301-code-challenges/two-dimensional-arrays/challenges-12.test.js
+++ code-challenges/301-code-challenges/two-dimensional-arrays/challenges-12.test.js
@@ -24,7 +24,7 @@ const cookieStores = [firstPike, seaTac, seattleCenter, capHill, alkiBeach];
 
 const grandTotal = (stores) => {
 
-  let hourlyCookieTotals = [];
+  let hourlySales = [];
 
   for (let i = 0; i < stores[0].length; i++) {
     var count = 0;
@@ -33,10 +33,10 @@ const grandTotal = (stores) => {
       count = stores[j][i] + count;
       console.log('count', count);
     }
-    hourlyCookieTotals.push(count);
-    console.log('hourlyCookieTotals', hourlyCookieTotals);
+    hourlySales.push(count);
+    console.log('hourlySales', hourlySales);
   }
-  return hourlyCookieTotals;
+  return hourlySales;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -50,8 +50,37 @@ Write a function named salesData that uses forEach to iterate over the hourlySal
 ------------------------------------------------------------------------------------------------ */
 
 const salesData = (hours, data) => {
-  // Solution code here...
+  const formattedDataArray = [];
+  data.forEach((elem, idx) => {
+    let salesDataObject = {
+      sales: `${elem} cookies`,
+      time: `${hours[idx]}`,
+    };
+    formattedDataArray.push(salesDataObject);
+  });
+  return formattedDataArray;
 };
+
+describe('Testing challenge 2', () => {
+  test('It should create an object of data for each store', () => {
+    expect(salesData(hoursOpen, grandTotal(cookieStores))).toStrictEqual([
+      { sales: '88 cookies', time: '9 a.m.' },
+      { sales: '153 cookies', time: '10 a.m.' },
+      { sales: '252 cookies', time: '11 a.m.' },
+      { sales: '286 cookies', time: '12 p.m.' },
+      { sales: '139 cookies', time: '1 p.m.' },
+      { sales: '161 cookies', time: '2 p.m.' },
+      { sales: '145 cookies', time: '3 p.m.' },
+      { sales: '232 cookies', time: '4 p.m.' },
+      { sales: '276 cookies', time: '5 p.m.' },
+      { sales: '207 cookies', time: '6 p.m.' },
+      { sales: '161 cookies', time: '7 p.m.' },
+      { sales: '169 cookies', time: '8 p.m.' }
+    ]);
+
+    expect(salesData(hoursOpen, grandTotal(cookieStores)).length).toStrictEqual(hoursOpen.length);
+  });
+});
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 3
