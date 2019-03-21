d04af1a95691508538f8d11660604064730979a0
Andrew Curtis <curtis.andrewm@gmail.com>
2019-03-21 10:13:39 -0700

    Add initial setup of console app for BinarySearch algorithm

---
+++ code-challenges/c-sharp/array-binary-search/BinarySearch/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the binary search challenge!");
+            Console.WriteLine(" ");
+
+            int[] inputArray = { 4, 8, 15, 16, 19, 20, 23, 26, 29, 34, 39, 42 };
+            Console.WriteLine("The array we will search to find our key value is {0}", string.Join(", ", inputArray));
+
+            int key = 15;
+            Console.WriteLine($"The key value we will search for is {key}.");
+
+            // Save result of BinarySearch algorithm method
+            int result = BinarySearch(inputArray, key);
+
+            if (result == -1)
+            {
+                Console.WriteLine($"The element we searched for ({key}) is not present in the array we searched in.");
+            }
+            else
+            {
+                Console.WriteLine($"The index of the array’s element that is equal to our search key is {result}");
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine("Press Enter to exit the program.");
+            Console.ReadLine();
+        }
+
+        public static int BinarySearch(int[] sortedArray, int key)
+        {
+            int min = 0;
+            int max = sortedArray.Length;
+
+
+            return key;
+        }
+    }
+}
--- code-challenges/c-sharp/array-binary-search/README.md
+++ code-challenges/c-sharp/array-binary-search/README.md
@@ -2,9 +2,14 @@
 
 ## Challenge
 
+Write a static method called BinarySearch that takes in two parameters: a sorted array and a 
+search key. Without utilizing any of the built-in methods available to in your language, 
+return the index of the array's element that is equal to the search key, or -1 if the 
+element is not present in the array.
 
 ## Approach & Efficiency
 
 
 ## Solution
 
+![Binary Search Whiteboard Solution](https://github.com/amjcurtis/data-structures-and-algorithms/blob/master/assets/binary-search-whiteboard.JPG)
