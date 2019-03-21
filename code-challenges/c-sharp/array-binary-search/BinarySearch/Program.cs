035f2bee9d8ef572b479f1e596fa35ca141d788c
Andrew Curtis <curtis.andrewm@gmail.com>
2019-03-21 10:49:53 -0700

    Allow user to specify key value to search for; add exception handling for user input

--- code-challenges/c-sharp/array-binary-search/BinarySearch/Program.cs
+++ code-challenges/c-sharp/array-binary-search/BinarySearch/Program.cs
@@ -9,27 +9,53 @@ namespace BinarySearch
             Console.WriteLine("Welcome to the binary search challenge!");
             Console.WriteLine(" ");
 
-            int[] inputArray = { 4, 8, 15, 16, 19, 20, 23, 26, 29, 34, 39, 42 };
-            Console.WriteLine("The array we will search to find our key value is {0}", string.Join(", ", inputArray));
-
-            int key = 15;
-            Console.WriteLine($"The key value we will search for is {key}.");
-
-            // Save result of BinarySearch algorithm method
-            int result = BinarySearch(inputArray, key);
-
-            if (result == -1)
+            try
             {
-                Console.WriteLine($"The index returned by our algorithm is [{result}]");
-                Console.WriteLine($"This means the element we searched for ({key}) is not present in the array we searched in.");
+                Console.WriteLine("Enter a number you'd like to search for in an array.");
+                string chosenNumberAsString = Console.ReadLine();
+
+                int chosenNumberAsInt = Convert.ToInt32(chosenNumberAsString);
+
+                // Set key value to search for
+                int key = chosenNumberAsInt;
+                Console.WriteLine($"Ok, the key value we will search for is {key}.");
+
+                // Declare sorted array to search in
+                int[] sortedInputArray = { 4, 8, 15, 16, 19, 20, 23, 26, 29, 34, 39, 42 };
+
+                // Save result of BinarySearch algorithm method
+                int result = BinarySearch(sortedInputArray, key);
+
+                if (result == -1)
+                {
+                    Console.WriteLine($"The index returned by our algorithm is [{result}]");
+                    Console.WriteLine($"This means the element we searched for ({key}) is not present in the array we searched in.");
+                }
+                else
+                {
+                    Console.WriteLine($"The index of the array element that is equal to our search key is [{result}]");
+                }
+
+                Console.WriteLine("Here's the array we searched for our number in:\n" +
+                    "{0}", string.Join(", ", sortedInputArray));
+
+                Console.WriteLine(" ");
+                Console.WriteLine("Thanks for trying out our algorithm!");
             }
-            else
+            catch (FormatException e)
             {
-                Console.WriteLine($"The index of the array element that is equal to our search key is [{result}]");
+                Console.WriteLine(e.Message);
             }
-            Console.WriteLine(" ");
-            Console.WriteLine("Press Enter to exit the program.");
-            Console.ReadLine();
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("Press Enter to exit the program.");
+                Console.ReadLine();
+            }
         }
 
         public static int BinarySearch(int[] array, int key)
