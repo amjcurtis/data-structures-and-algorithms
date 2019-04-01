8125e1bebde7cfe44521fce90b275c00cde1bb9e
Andrew Curtis <curtis.andrewm@gmail.com>
2019-03-31 23:38:14 -0700

    Add remaining unit tests (failing for tests that depend on Enqueue and Dequeue methods)

--- data-structures/StacksAndQueues/XUnit_StacksAndQueues/UnitTest1.cs
+++ data-structures/StacksAndQueues/XUnit_StacksAndQueues/UnitTest1.cs
@@ -23,6 +23,23 @@ namespace XUnit_StacksAndQueues
         }
 
         /// <summary>
+        /// Tests Stack's Push method for multiple successive nodes
+        /// </summary>
+        [Fact]
+        public void CanPushMultipeNodeSToStack()
+        {
+            // Arrange
+            Stack<Node<int>> myStack = new Stack<Node<int>>();
+
+            // Act
+            myStack.Push(25);
+            myStack.Push(16);
+
+            // Assert
+            Assert.Equal(16, myStack.Top.Value);
+        }
+
+        /// <summary>
         /// Tests Stack's Pop method
         /// </summary>
         [Fact]
@@ -38,6 +55,38 @@ namespace XUnit_StacksAndQueues
 
             // Assert
             Assert.Equal(25, poppedValue);
+        }
+
+        /// <summary>
+        /// Tests Stack's Pop method's ability to pop all nodes off stack
+        /// </summary>
+        [Fact]
+        public void CanPopAllNodesFromStack()
+        {
+            // Arrange
+            Stack<Node<int>> myStack = new Stack<Node<int>>();
+            myStack.Push(12);
+            myStack.Push(25);
+
+            // Act
+            myStack.Pop();
+            myStack.Pop();
+
+            // Assert
+            Assert.Null(myStack.Top);
+        }
+
+        /// <summary>
+        /// Tests whether can instantiate an empty stack
+        /// </summary>
+        [Fact]
+        public void CanInstantiateEmptyStack()
+        {
+            // Act
+            Stack<Node<int>> myStack = new Stack<Node<int>>();
+
+            // Assert
+            Assert.Null(myStack.Top);
         }
 
         /// <summary>
@@ -74,6 +123,23 @@ namespace XUnit_StacksAndQueues
         }
 
         /// <summary>
+        /// Tests Queue's Enqueue method can enqueue multiple nodes
+        /// </summary>
+        [Fact]
+        public void CanEnqueueMultipleNodesInQueue()
+        {
+            // Arrange
+            Queue<Node<int>> myQueue = new Queue<Node<int>>();
+
+            // Act
+            myQueue.Enqueue(20);
+            myQueue.Enqueue(13);
+
+            // Assert
+            Assert.Equal(13, myQueue.Rear.Value);
+        }
+
+        /// <summary>
         /// Tests Queue's Dequeue method
         /// </summary>
         [Fact]
@@ -92,6 +158,25 @@ namespace XUnit_StacksAndQueues
         }
 
         /// <summary>
+        /// Tests Queue's Dequeue method can empty a queue
+        /// </summary>
+        [Fact]
+        public void CanDequeueAllNodesFromQueue()
+        {
+            // Arrange
+            Queue<Node<int>> myQueue = new Queue<Node<int>>();
+            myQueue.Enqueue(25);
+            myQueue.Enqueue(16);
+
+            // Act
+            myQueue.Dequeue();
+            myQueue.Dequeue();
+
+            // Assert
+            Assert.Null(myQueue.Front);
+        }
+
+        /// <summary>
         /// Tests Queue's Peek method
         /// </summary>
         [Fact]
@@ -106,6 +191,19 @@ namespace XUnit_StacksAndQueues
 
             // Assert
             Assert.Equal(25, valOfPeekedFrontNode);
+        }
+
+        /// <summary>
+        /// Tests whether can instantiate empty queue
+        /// </summary>
+        [Fact]
+        public void CanInstantiateEmptyQueue()
+        {
+            // Act
+            Queue<Node<int>> myQueue = new Queue<Node<int>>();
+
+            // Assert
+            Assert.Null(myQueue.Front);
         }
     }
 }
