8b3fa727de4736fd8f5c1a41c0fadc25259ddc6b
Andrew Curtis <curtis.andrewm@gmail.com>
2019-04-02 22:36:34 -0700

    Add further minor edits

--- code-challenges/c-sharp/FIFOAnimalShelter/FIFOAnimalShelter/Classes/AnimalShelter.cs
+++ code-challenges/c-sharp/FIFOAnimalShelter/FIFOAnimalShelter/Classes/AnimalShelter.cs
@@ -5,10 +5,19 @@ namespace FIFOAnimalShelter.Classes
 {
     public class AnimalShelter<T>
     {
+        // Added these back in after reimplementing AnimalShelter constructor
+        public Node<T> Front { get; set; }
+        public Node<T> Rear { get; set; }
 
         // Publicly accessible queue
         public Queue<T> AnimalQueue = new Queue<T>();
        
+        // Added this back in after determining Queue class's Enqueue and Dequeue methods were being used instead of AnimalShelter's
+        public AnimalShelter() // Tried adding this parameter: Node<T> node, but didn't solve NullRefExc
+        {
+            AnimalQueue = new Queue<T>(); // Tried passing 'node' arg in per above comment, but didn't solve NullRefExc
+        }
+
         /// <summary>
         /// Adds new node with string value "dog" or "cat" to end of queue
         /// </summary>
@@ -16,8 +25,8 @@ namespace FIFOAnimalShelter.Classes
         public void Enqueue(T animal)
         {
             Node<T> animalToEnqueue = new Node<T>(animal);
-            AnimalQueue.Rear.Next = animalToEnqueue;
-            AnimalQueue.Rear = animalToEnqueue;
+            Rear.Next = animalToEnqueue;
+            Rear = animalToEnqueue;
         }
 
         /// <summary>
@@ -25,33 +34,33 @@ namespace FIFOAnimalShelter.Classes
         /// </summary>
         /// <param name="pref">string specifying user's preferred type of animal</param>
         /// <returns>first node with value of user's preferred type of animal</returns>
-        public Node<T> Dequeue(Node<T> pref)
-        {
-            // Input validation
-            if ( !pref.Value.Equals("dog") && !pref.Value.Equals("cat") )
-            {
-                return null;
-            }
+        //public Node<T> Dequeue(Node<T> pref)
+        //{
+        //    // Input validation
+        //    if ( !pref.Value.Equals("dog") && !pref.Value.Equals("cat") )
+        //    {
+        //        return null;
+        //    }
 
-            Queue<T> queue2 = new Queue<T>();
-            Node<T> temp = null;
+        //    Queue<T> queue2 = new Queue<T>();
+        //    Node<T> temp = null;
 
-            if ( pref.Value.Equals(AnimalQueue.Peek().Value) )
-            {
-                return AnimalQueue.Peek();
-            }
-            while (AnimalQueue.Front.Next != null)
-            {
-                if ( AnimalQueue.Front.Value.Equals(pref.Value) )
-                {
-                    temp = AnimalQueue.Front;
-                }
-                Node<T> temp2 = AnimalQueue.Front;
-                AnimalQueue.Front = AnimalQueue.Front.Next;
-                queue2.Enqueue(temp2.Value);
-            }
-            return temp;
-        }
+        //    if ( pref.Value.Equals(AnimalQueue.Peek().Value) )
+        //    {
+        //        return AnimalQueue.Peek();
+        //    }
+        //    while (AnimalQueue.Front.Next != null)
+        //    {
+        //        if ( AnimalQueue.Front.Value.Equals(pref.Value) )
+        //        {
+        //            temp = AnimalQueue.Front;
+        //        }
+        //        Node<T> temp2 = AnimalQueue.Front;
+        //        AnimalQueue.Front = AnimalQueue.Front.Next;
+        //        queue2.Enqueue(temp2.Value);
+        //    }
+        //    return temp;
+        //}
 
 
     }
--- code-challenges/c-sharp/FIFOAnimalShelter/FIFOAnimalShelter/Program.cs
+++ code-challenges/c-sharp/FIFOAnimalShelter/FIFOAnimalShelter/Program.cs
@@ -8,27 +8,53 @@ namespace FIFOAnimalShelter
     {
         public static void Main(string[] args)
         {
+            ////////////////////////////////////////////
+            // Section below uses *Queue* class's methods
+            ////////////////////////////////////////////
+            //Node<string> newAnimal = new Node<string>("dog");
+            //Queue<string> animalQueue = new Queue<string>(newAnimal);
+            //for (int i = 1; i <= 6; i++)
+            //{
+            //    if (i % 3 == 0)
+            //    {
+            //        animalQueue.Enqueue("cat");
+            //    }
+            //    else
+            //    {
+            //        animalQueue.Enqueue("dog");
+            //    }
+            //}
 
-            Node<string> newAnimal = new Node<string>("dog");
-            Queue<string> animalQueue = new Queue<string>(newAnimal);
+            //Console.WriteLine(animalQueue.Front.Value);
+            //Console.WriteLine(animalQueue.Rear.Value);
+
+            //Node<string> preferredAnimal = new Node<string>("cat");
+            // Doesn't work; uses Queue class's Dequeue method, not AnimalShelter class's Dequeue
+            //animalQueue.Dequeue(preferredAnimal);
+
+
+            ////////////////////////////////////////////
+            // Section below uses *AnimalShelter* class's methods
+            ////////////////////////////////////////////
+
+            Node<string> catPreference = new Node<string>("cat");
+            Node<string> dogPreference = new Node<string>("dog");
+
+            AnimalShelter<string> animalShelterQueue = new AnimalShelter<string>(catPreference);
             for (int i = 1; i <= 6; i++)
             {
                 if (i % 3 == 0)
                 {
-                    animalQueue.Enqueue("cat");
+                    animalShelterQueue.Enqueue("cat");
                 }
                 else
                 {
-                    animalQueue.Enqueue("dog");
+                    animalShelterQueue.Enqueue("dog");
                 }
             }
 
-            Console.WriteLine(animalQueue.Front.Value);
-            Console.WriteLine(animalQueue.Rear.Value);
-            
-            Node<string> preferredAnimal = new Node<string>("cat");
-            animalQueue.Dequeue(preferredAnimal);
-            
+            //animalShelterQueue.Dequeue(catPreference);
+
             Console.ReadLine();
         }
     }
