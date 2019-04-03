caf82a7e362b87741347a3fc30f760b581a38ffc
Andrew Curtis <curtis.andrewm@gmail.com>
2019-04-02 23:24:29 -0700

    Add passing tests for happy path and expected failure

--- code-challenges/c-sharp/QueueWithStacks/QueueWithStacks/Program.cs
+++ code-challenges/c-sharp/QueueWithStacks/QueueWithStacks/Program.cs
@@ -9,14 +9,12 @@ namespace QueueWithStacks
     {
         static void Main(string[] args)
         {
-            Node<int> newNode = new Node<int>(21);
             PseudoQueue<int> pseudoQueue = new PseudoQueue<int>();
 
-            for (int i = 0; i < 6; i++)
-            {
-                pseudoQueue.Enqueue(i + 1);
-            }
-            //Console.WriteLine($"pseudoQueue.Front: {pseudoQueue.Top}");
+            //for (int i = 0; i < 6; i++)
+            //{
+            //    pseudoQueue.Enqueue(i + 1);
+            //}
             Console.WriteLine($"Value of pseudoQueue.Dequeue(): {pseudoQueue.Dequeue().Value}");
             Console.ReadLine();
         }
