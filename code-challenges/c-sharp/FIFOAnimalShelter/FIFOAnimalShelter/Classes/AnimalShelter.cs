eb7ec50e2131e746a36c9a9bf940e02604bb5b3a
Andrew Curtis <curtis.andrewm@gmail.com>
2019-04-03 00:59:12 -0700

    Add full Readme documentation and uncomment (not yet working) Dequeue() method

--- code-challenges/c-sharp/FIFOAnimalShelter/FIFOAnimalShelter/Classes/AnimalShelter.cs
+++ code-challenges/c-sharp/FIFOAnimalShelter/FIFOAnimalShelter/Classes/AnimalShelter.cs
@@ -34,33 +34,33 @@ namespace FIFOAnimalShelter.Classes
         /// </summary>
         /// <param name="pref">string specifying user's preferred type of animal</param>
         /// <returns>first node with value of user's preferred type of animal</returns>
-        //public Node<T> Dequeue(Node<T> pref)
-        //{
-        //    // Input validation
-        //    if ( !pref.Value.Equals("dog") && !pref.Value.Equals("cat") )
-        //    {
-        //        return null;
-        //    }
+        public Node<T> Dequeue(Node<T> pref)
+        {
+            // Input validation
+            if (!pref.Value.Equals("dog") && !pref.Value.Equals("cat"))
+            {
+                return null;
+            }
 
-        //    Queue<T> queue2 = new Queue<T>();
-        //    Node<T> temp = null;
+            Queue<T> queue2 = new Queue<T>();
+            Node<T> temp = null;
 
-        //    if ( pref.Value.Equals(AnimalQueue.Peek().Value) )
-        //    {
-        //        return AnimalQueue.Peek();
-        //    }
-        //    while (AnimalQueue.Front.Next != null)
-        //    {
-        //        if ( AnimalQueue.Front.Value.Equals(pref.Value) )
-        //        {
-        //            temp = AnimalQueue.Front;
-        //        }
-        //        Node<T> temp2 = AnimalQueue.Front;
-        //        AnimalQueue.Front = AnimalQueue.Front.Next;
-        //        queue2.Enqueue(temp2.Value);
-        //    }
-        //    return temp;
-        //}
+            if (pref.Value.Equals(AnimalQueue.Peek().Value))
+            {
+                return AnimalQueue.Peek();
+            }
+            while (AnimalQueue.Front.Next != null)
+            {
+                if (AnimalQueue.Front.Value.Equals(pref.Value))
+                {
+                    temp = AnimalQueue.Front;
+                }
+                Node<T> temp2 = AnimalQueue.Front;
+                AnimalQueue.Front = AnimalQueue.Front.Next;
+                queue2.Enqueue(temp2.Value);
+            }
+            return temp;
+        }
 
 
     }
