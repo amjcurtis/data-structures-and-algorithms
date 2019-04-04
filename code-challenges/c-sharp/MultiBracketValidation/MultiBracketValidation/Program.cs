ef392322a1bc3ccac4deb373369c8c6a5da51a7b
Andrew Curtis <curtis.andrewm@gmail.com>
2019-04-04 00:38:17 -0700

    Add comments/planning and all required unit tests

--- code-challenges/c-sharp/MultiBracketValidation/MultiBracketValidation/Program.cs
+++ code-challenges/c-sharp/MultiBracketValidation/MultiBracketValidation/Program.cs
@@ -2,11 +2,59 @@
 
 namespace MultiBracketValidation
 {
-	class Program
+	public class Program
 	{
-		static void Main(string[] args)
+		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			
+		}
+
+		public static bool MultiBracketValidation(string input)
+		{
+			/* 
+			//TODO
+			Split input string into char array
+			
+			// Need logic somewhere to return false for cases of overlapping bracket units like 3rd false InlineData test
+
+			for loop over each char in array
+					
+				// Need to ensure an opening bracket doesn't come before first closing bracket
+				if (char == ')' && stack1.Top == null
+					return false
+				else if char == '}' && stack2.Top == null
+					return false
+				else if char == ']' && stack3.Top == null
+					return false
+				else
+					continue
+
+					
+				if char at index is '('
+					push to paren stack
+				if char at index is '{'
+					push to curly brace stack
+				if char at index is '['
+					push to square bracket stack
+
+
+			for loop over array a second time
+				if char at index is ')'
+					pop from paren stack
+				if char at index is '}'
+					pop from curly brace stack
+				if char at index is ']'
+					pop from square bracket stack
+			
+			
+			if stack1.Top or stack2.Top or stack3.Top != null
+				return false
+			else
+				return true
+					
+			*/
+
+
 		}
 	}
 }
--- code-challenges/c-sharp/MultiBracketValidation/XUnit_MultiBracketValidation/UnitTest1.cs
+++ code-challenges/c-sharp/MultiBracketValidation/XUnit_MultiBracketValidation/UnitTest1.cs
@@ -5,20 +5,35 @@ namespace XUnit_MultiBracketValidation
 {
 	public class UnitTest1
 	{
+
+		/// <summary>
+		/// Tests whether MultiBracketValidation method can correctly return true or false
+		/// </summary>
+		/// <param name="pattern"></param>
+		/// <param name="expectedBool"></param>
 		[Theory]
+		// Return true
 		[InlineData("{}", true)]
 		[InlineData("{}(){}", true)]
 		[InlineData("()[[Extra Characters]]", true)]
 		[InlineData("(){}[[]]", true)]
 		[InlineData("{}{Code}[Fellows](())", true)]
+		// Return false
 		[InlineData("[({}]", false)]
 		[InlineData("(](", false)]
 		[InlineData("{(})", false)]
 		[InlineData("{", false)]
-		[InlineData(")", false)]
 		[InlineData("[}", false)]
-		public void Test1(string pattern, bool expectedBool)
+		// Edge case: first bracket in string is a closing bracket
+		[InlineData(")", false)]
+
+		public void CanMultiBracketValidationMethodCorrectlyReturnTrueAndFalse(string pattern, bool expectedBool)
 		{
+			// Arrange
+
+			// Act
+
+			// Assert
 
 		}
 	}
