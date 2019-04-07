9b612b1a163566493deaf01cb287b947deaf1552
Andrew Curtis <curtis.andrewm@gmail.com>
2019-04-06 19:17:09 -0700

    Add additional classes, start on PreOrder and helper method

---
+++ data-structures/BinaryTree/BinaryTree/Classes/BinaryTree.cs
@@ -0,0 +1,43 @@
+﻿using BinaryTree.Classes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTree
+{
+	public class BinaryTree<T>
+	{
+		public Node<T> Root { get; set; }
+		
+		// Array for storing values to be output from methods below
+		T[] output = new T[15];
+
+		// Count to use for tracking number of values in output array
+		int count;
+
+		/// <summary>
+		/// Performs depth-first traversal of binary tree, inspecting root node before traversing to child nodes
+		/// </summary>
+		/// <param name="root">root node of tree</param>
+		/// <returns>array of type T</returns>
+		public T[] PreOrder(Node<T> root)
+		{
+			// Clear array (reset all indices to default value)
+			Array.Clear(output, 0, output.Length);
+
+
+		} 
+
+		// Helper method for pushing new values to output array
+		public void AddToArray(T value)
+		{
+			if (count == output.Length)
+			{
+				Array.Resize(ref output, output.Length + (output.Length / 2));
+			}
+
+			// Updates output array at next index
+			output[count++] = value;
+		}
+	}
+}
---
+++ data-structures/BinaryTree/BinaryTree/Classes/Node.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTree.Classes
+{
+	public class Node<T>
+	{
+		public T Value { get; set; }
+		public Node<T> LeftChild { get; set; }
+		public Node<T> RightChild { get; set; }
+
+		// Node constructor
+		public Node(T value)
+		{
+			Value = value;
+			LeftChild = null;
+			RightChild = null;
+		}
+
+		// Empty constructor so can instantiate empty node
+		public Node()
+		{
+		}
+	}
+}
