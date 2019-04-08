dcd34867cde9a6e1f9ff762a5aea6c16432fab5e
Andrew Curtis <curtis.andrewm@gmail.com>
2019-04-08 09:45:28 -0700

    Add passing second unit test

--- data-structures/BinaryTree/XUnit_BinaryTree/UnitTest1.cs
+++ data-structures/BinaryTree/XUnit_BinaryTree/UnitTest1.cs
@@ -6,6 +6,10 @@ namespace XUnit_BinaryTree
 {
 	public class UnitTest1
 	{
+
+		/// <summary>
+		/// Tests whether can instantiate empty binary tree with no nodes
+		/// </summary>
 		[Fact]
 		public void CanInstantiateEmptyTree()
 		{
@@ -13,6 +17,18 @@ namespace XUnit_BinaryTree
 			BinaryTree<int> tree = new BinaryTree<int>();
 
 			Assert.Null(tree.Root);
+		}
+
+		/// <summary>
+		/// Tests whether can instantiate tree and add single root node
+		/// </summary>
+		[Fact]
+		public void CanInstantiateTreeWithSingleNode()
+		{
+			Node<int> intNode = new Node<int>(1);
+			BinaryTree<int> tree = new BinaryTree<int>(intNode);
+
+			Assert.Equal(1, tree.Root.Value);
 		}
 	}
 }
