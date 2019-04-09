4aaa41cfce12fa76cf624deda399e55cd42af7e2
Andrew Curtis <curtis.andrewm@gmail.com>
2019-04-09 00:43:34 -0700

    Add initial work on FizzBuzzTree method

--- code-challenges/c-sharp/FizzBuzzTree/FizzBuzzTree/Program.cs
+++ code-challenges/c-sharp/FizzBuzzTree/FizzBuzzTree/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using BinaryTree.Classes;
 
 namespace FizzBuzzTree
 {
-	class Program
+	public class Program
 	{
-		static void Main(string[] args)
+		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			BinaryTree<int> tree = new BinaryTree<int>();
+		}
+
+		public BinaryTree<object> FizzBuzzTree(BinaryTree<int> tree)
+		{
+			// FizzBuzz tests
+			if (tree.Root.Value % 3 == 0 && tree.Root.Value % 5 == 0)
+			{
+				tree.Root.Value == "FizzBuzz";
+			}
+			else if (tree.Root.Value % 3 == 0)
+			{
+				tree.Root.Value == "Fizz";
+			}
+			else if (tree.Root.Value % 5 == 0)
+			{
+				tree.Root.Value == "Buzz";
+			}
+			
+			// Traversal of child nodes
+				if (tree.Root.LeftChild != null)
+			{
+				FizzBuzzTree(tree.Root.LeftChild);
+			}
+
+			if (tree.Root.RightChild != null)
+			{
+				FizzBuzzTree(tree.Root.RightChild);
+			}
+
+			return tree;
 		}
 	}
 }
