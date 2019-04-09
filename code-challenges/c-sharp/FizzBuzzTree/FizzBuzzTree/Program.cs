5167544870a6354d64c1683b6d5111cd52eb2600
Andrew Curtis <curtis.andrewm@gmail.com>
2019-04-09 11:55:57 -0700

    Add further work on fizzbuzztree

--- code-challenges/c-sharp/FizzBuzzTree/FizzBuzzTree/Program.cs
+++ code-challenges/c-sharp/FizzBuzzTree/FizzBuzzTree/Program.cs
@@ -7,37 +7,72 @@ namespace FizzBuzzTree
 	{
 		public static void Main(string[] args)
 		{
-			BinaryTree<int> tree = new BinaryTree<int>();
+			BinaryTree<object> tree = new BinaryTree<object>();
+
+			Node<object> node1 = new Node<object>(1);
+			Node<object> node2 = new Node<object>(2);
+			Node<object> node3 = new Node<object>(3);
+			Node<object> node4 = new Node<object>(4);
+			Node<object> node5 = new Node<object>(5);
+			Node<object> node6 = new Node<object>(6);
+			Node<object> node7 = new Node<object>(7);
+			Node<object> node8 = new Node<object>(8);
+			Node<object> node9 = new Node<object>(9);
+			Node<object> node10 = new Node<object>(10);
+
+			tree.Root = node1;
+			tree.Root.LeftChild = node2;
+			tree.Root.RightChild = node3;
+			tree.Root.LeftChild.LeftChild = node4;
+			tree.Root.LeftChild.RightChild = node5;
+			tree.Root.RightChild.LeftChild = node6;
+			tree.Root.RightChild.RightChild = node7;
+			tree.Root.LeftChild.LeftChild.LeftChild = node8;
+			tree.Root.LeftChild.LeftChild.RightChild = node9;
+			tree.Root.LeftChild.RightChild.LeftChild = node10;
+
+			FizzBuzzTree(tree);
+
+			Console.WriteLine($"tree.Root.RightChild: {tree.Root.RightChild.Value}");
+
+			Console.ReadLine();
 		}
 
-		public BinaryTree<object> FizzBuzzTree(BinaryTree<int> tree)
+		public static BinaryTree<object> FizzBuzzTree(BinaryTree<object> tree)
 		{
 			// FizzBuzz tests
-			if (tree.Root.Value % 3 == 0 && tree.Root.Value % 5 == 0)
+			if ((int)tree.Root.Value % 3 == 0 && (int)tree.Root.Value % 5 == 0)
 			{
-				tree.Root.Value == "FizzBuzz";
+				tree.Root.Value = "FizzBuzz";
 			}
-			else if (tree.Root.Value % 3 == 0)
+			else if ((int)tree.Root.Value % 3 == 0)
 			{
-				tree.Root.Value == "Fizz";
+				tree.Root.Value = "Fizz";
 			}
-			else if (tree.Root.Value % 5 == 0)
+			else if ((int)tree.Root.Value % 5 == 0)
 			{
-				tree.Root.Value == "Buzz";
+				tree.Root.Value = "Buzz";
 			}
 			
 			// Traversal of child nodes
-				if (tree.Root.LeftChild != null)
+			if (tree.Root.LeftChild != null)
 			{
-				FizzBuzzTree(tree.Root.LeftChild);
+				tree.PreOrder(tree.Root.LeftChild);
 			}
 
 			if (tree.Root.RightChild != null)
 			{
-				FizzBuzzTree(tree.Root.RightChild);
+				tree.PreOrder(tree.Root.RightChild);
 			}
 
 			return tree;
 		}
+
+		// Helper method to check for FizzBuzz
+		//public string CheckFizzBuzz(int value)
+		//{
+
+		//}
+
 	}
 }
