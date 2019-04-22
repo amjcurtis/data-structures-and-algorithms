06a61e5ee549ba562cd05e4d4c8d2cd92c0a3a34
Andrew Curtis <curtis.andrewm@gmail.com>
2019-04-22 09:46:09 -0700

    Add initial VS projects for insertion sort

---
+++ sorts/InsertionSort/InsertionSort/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace InsertionSort
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+
+		/// <summary>
+		/// Sorts an array of integers using insertion sort
+		/// </summary>
+		/// <param name="inputArray">array of integers</param>
+		/// <returns>sorted integer array</returns>
+		static int[] InsertionSort(int[] inputArray)
+		{
+
+		}
+	}
+}
---
+++ sorts/InsertionSort/XUnit_InsertionSort/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace XUnit_InsertionSort
+{
+	public class UnitTest_InsertionSort
+	{
+		[Fact]
+		public void Test1()
+		{
+
+		}
+	}
+}
