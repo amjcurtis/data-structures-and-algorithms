0e6cbd19f4c0a5b772fb1a8604b0eee829031b07
Andrew Curtis <curtis.andrewm@gmail.com>
2019-04-22 10:49:21 -0700

    Add working InsertionSort method and passing unit tests

--- sorts/InsertionSort/InsertionSort/Program.cs
+++ sorts/InsertionSort/InsertionSort/Program.cs
@@ -2,11 +2,27 @@
 
 namespace InsertionSort
 {
-	class Program
+	public class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int[] array = new int[] { 4, 3, 1, 8, 5, 7, 2 };
+
+			int[] sortedArray = InsertionSort(array);
+
+			for (int i = 0; i < sortedArray.Length; i++)
+			{
+				Console.Write("{0}  ", sortedArray[i]);
+			}
+
+			int[] emptyArray = new int[0];
+
+			int[] sortedEmptyArray = InsertionSort(emptyArray);
+
+			Console.WriteLine("");
+			Console.WriteLine(emptyArray[0]);
+
+			Console.ReadLine();
 		}
 
 		/// <summary>
@@ -14,9 +30,28 @@ namespace InsertionSort
 		/// </summary>
 		/// <param name="inputArray">array of integers</param>
 		/// <returns>sorted integer array</returns>
-		static int[] InsertionSort(int[] inputArray)
+		public static int[] InsertionSort(int[] inputArray)
 		{
+			if (inputArray.Length == 0)
+			{
+				return inputArray;
+			}
 
+			for (int i = 1; i < inputArray.Length; i++)
+			{
+				int j = i - 1;
+				int temp = inputArray[i];
+
+				while (j >= 0 && temp < inputArray[j])
+				{
+					inputArray[j + 1] = inputArray[j];
+					j = j - 1;
+				}
+
+				inputArray[j + 1] = temp;
+			}
+
+			return inputArray;
 		}
 	}
 }
