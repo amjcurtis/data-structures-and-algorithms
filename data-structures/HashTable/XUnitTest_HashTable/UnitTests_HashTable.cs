bd767cf31d23cfdab08a8f9406d46fad71c88a52
Andrew Curtis <curtis.andrewm@gmail.com>
2019-04-26 23:59:05 -0700

    Add planning comments for unit tests

--- data-structures/HashTable/XUnitTest_HashTable/UnitTests_HashTable.cs
+++ data-structures/HashTable/XUnitTest_HashTable/UnitTests_HashTable.cs
@@ -10,5 +10,14 @@ namespace XUnitTest_HashTable
 		{
 
 		}
+
+		//TODO Test that methods work by...
+
+		//TODO Adding a key/value to your hashtable results in the value being in the data structure
+		//TODO Retrieving based on a key returns the value stored
+		//TODO DSuccessfully returns null for a key that does not exist in the hashtable
+		//TODO Successfully handle a collision within the hashtable
+		//TODO Successfully retrieve a value from a bucket within the hashtable that has a collision
+		//TODO Successfully hash a key to an in-range value
 	}
 }
