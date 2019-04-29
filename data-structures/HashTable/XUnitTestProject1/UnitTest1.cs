1332eba3e99bb651cd1ace1196d2f2f7c6bca04f
Andrew Curtis <curtis.andrewm@gmail.com>
2019-04-28 23:29:37 -0700

    Add final passing tests

--- data-structures/HashTable/XUnitTestProject1/UnitTest1.cs
+++ data-structures/HashTable/XUnitTestProject1/UnitTest1.cs
@@ -54,7 +54,74 @@ namespace XUnitTestProject1
 
 			// Assert
 			Assert.Null(node);
+		}
 
+		/// <summary>
+		/// Tests whether can add multiple key:value pairs with same hash value to table
+		/// </summary>
+		[Fact]
+		public void CanHandleCollision()
+		{
+			// Arrange
+			Hashtable ht = new Hashtable(16);
+
+			// Act
+			ht.Add("John", "Instructor");
+			ht.Add("Joim", "Friend");
+
+			int nodeCount = 0;
+			for (int i = 0; i < ht.Map.Length; i++)
+			{
+				if (ht.Map[i] != null)
+				{
+					foreach (var item in ht.Map[i])
+					{
+						if (item.Key == "John")
+						{
+							nodeCount = ht.Map[i].Count;
+						}
+
+					}
+				}
+			}
+
+			// Assert
+			Assert.Equal(2, nodeCount);
+		}
+
+		/// <summary>
+		/// Tests whether can get value of a collided node in table
+		/// </summary>
+		[Fact]
+		public void CanGetValueOfCollidedNode()
+		{
+			// Arrange
+			Hashtable ht = new Hashtable(16);
+
+			// Act
+			ht.Add("John", "Instructor");
+			ht.Add("Joim", "Friend");
+
+			Node node = ht.Get("Joim");
+
+			// Assert
+			Assert.Equal("Friend", node.Value);
+		}
+
+		/// <summary>
+		/// Tests whether can hash key to an in-range value
+		/// </summary>
+		[Fact]
+		public void CanHashKeyToAnInRangeValue()
+		{
+			// Arrange
+			Hashtable ht = new Hashtable(16);
+
+			// Act
+			int hash = ht.Hash("Pioneer Square");
+
+			// Assert
+			Assert.True(hash <= 16);
 		}
 	}
 }
