6e22c3ccbe5bd0e4603d4c43e9c142fced09bc64
Andrew Curtis <curtis.andrewm@gmail.com>
2019-04-28 21:32:08 -0700

    Add Node class; add HashTable methods Add(), Hash(), Print()

---
+++ data-structures/HashTable/HashTable/Classes/HashTable.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTable
+{
+	public class HashTable
+	{
+
+		private LinkedList<Node>[] Map { get; set; }
+
+		// Hash table constructor
+		public HashTable(int size)
+		{
+			Map = new LinkedList<Node>[size];
+		}
+
+		/// <summary>
+		/// Hashes key to integer
+		/// </summary>
+		/// <param name="key">key to hash</param>
+		/// <returns>integer hash</returns>
+		public int Hash(string key)
+		{
+			int hashValue = 0;
+
+			char[] letters = key.ToCharArray();
+
+			for (int i = 0; i < letters.Length; i++)
+			{
+				hashValue += letters[i];
+			}
+
+			hashValue = (hashValue * 599) % Map.Length;
+
+			return hashValue;
+		}
+
+		/// <summary>
+		/// Adds new hashed key:value pair to hash table
+		/// </summary>
+		/// <param name="key">key for key:value pair to add</param>
+		/// <param name="value">value for key:value pair to add</param>
+		public void Add(string key, string value)
+		{
+			int hashKey = Hash(key);
+
+			if (Map[hashKey] == null) 
+			{
+				Map[hashKey] = new LinkedList<Node>();
+			}
+
+			Map[hashKey].AddFirst(new Node(key, value));
+		}
+
+
+		//TODO Get() method
+		//public string Get(string key)
+		//{
+
+		//}
+
+
+		//TODO Contains() method
+		//public bool Contains(string key)
+		//{
+
+		//}
+
+
+		/// <summary>
+		/// Writes key:value pairs from hash table to console
+		/// </summary>
+		public void Print()
+		{
+			for (int i = 0; i < Map.Length; i++)
+			{
+				if (Map[i] != null)
+				{
+					Console.Write($"Bucket: {i}: ");
+					foreach (var item in Map[i])
+					{
+						Console.Write($"{item.Key}:{item.Value} --> ");
+					}
+				}
+				Console.Write("null");
+				Console.WriteLine();
+			}
+		}
+	}
+}
--- data-structures/HashTable/HashTable/Program.cs
+++ data-structures/HashTable/HashTable/Program.cs
@@ -6,7 +6,27 @@ namespace HashTable
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			// Initialize hash table of specified size
+			HashTable ht = new HashTable(1024);
+
+			// Populate hash table
+			ht.Add("John", "Husband");
+			ht.Add("Cathy", "Boss");
+			ht.Add("Amanda", "Instructor");
+			ht.Add("Allie", "Classmate");
+			ht.Add("Zach", "Friend");
+			ht.Add("Rosie", "Cat");
+			ht.Add("Justin", "Parrot");
+			ht.Add("Demi", "Dog");
+			ht.Add("Ovi", "Student");
+			ht.Add("Ben", "Student");
+			ht.Add("Khalil", "Student");
+			ht.Add("Jason", "Student");
+
+			// Print contents of hash table
+			ht.Print();
+
+			Console.ReadLine();
 		}
 	}
 }
