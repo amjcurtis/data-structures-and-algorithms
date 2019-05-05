4de38044057101269fa9c4eee8b28621264000a0
Andrew Curtis <curtis.andrewm@gmail.com>
2019-05-04 22:07:12 -0700

    Add initial GraphDSA and XUnit projects, graph classes, and first few graph methods

---
+++ data-structures/Graph/GraphDSA/Classes/Graph.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphDSA.Classes
+{
+	class Graph<T>
+	{
+
+		public Dictionary<Vertex<T>, List<Edge<T>>> AdjacencyList { get; set; }
+		private int _size = 0;
+
+		// Constructor
+		public Graph()
+		{
+			AdjacencyList = new Dictionary<Vertex<T>, List<Edge<T>>>();
+		}
+
+		// METHODS
+
+		
+		public Vertex<T> AddVertex(T value)
+		{
+			Vertex<T> vertex = new Vertex<T>(value);
+
+			AdjacencyList.Add(vertex, new List<Edge<T>>());
+
+			_size++;
+
+			return vertex;
+		}
+
+		public void AddDirectedEdge(Vertex<T> a, Vertex<T> b, int weight)
+		{
+			AdjacencyList[a].Add(
+
+				new Edge<T>
+				{
+					Weight = weight,
+					Vertex = b
+				}
+			);
+		}
+
+		public void AddUndirectedEdge(Vertex<T> a, Vertex<T> b, int weight)
+		{
+			AddDirectedEdge(a, b, weight);
+			AddDirectedEdge(b, a, weight);
+		}
+
+		/// <summary>
+		/// Returns iterable list of edges between specified vertex and adjacent vertices
+		/// </summary>
+		/// <param name="vertex">specified vertex</param>
+		/// <returns>list of edges</returns>
+		public List<Edge<T>> GetNeighbors(Vertex<T> vertex)
+		{
+			return AdjacencyList[vertex];
+		}
+
+
+
+		public void Print()
+		{
+			foreach (var vertex in AdjacencyList)
+			{
+				Console.Write($"Vertex: {vertex.Key.Value}  :  ");
+				foreach (var edge in vertex.Value)
+				{
+					Console.Write($"{edge.Vertex.Value}, {edge.Weight}  ->  ");
+				}
+				Console.WriteLine("NULL");
+			}
+		}
+	}
+}
---
+++ data-structures/Graph/GraphDSA/Program.cs
@@ -0,0 +1,25 @@
+﻿using GraphDSA.Classes;
+using System;
+
+namespace GraphDSA
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Graph<string> graph = new Graph<string>();
+
+			var a = graph.AddVertex("Detroit");
+			var b = graph.AddVertex("Boston");
+			var c = graph.AddVertex("Atlanta");
+
+			graph.AddUndirectedEdge(a, b, 6);
+			graph.AddUndirectedEdge(b, c, 10);
+			graph.AddDirectedEdge(a, c, 13);
+
+			graph.Print();
+
+			Console.ReadLine();
+		}
+	}
+}
