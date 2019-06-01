37cdad17c5c725b7630f11b3b584308bfdc6ca5b
Andrew Curtis <curtis.andrewm@gmail.com>
2019-06-01 15:10:28 -0700

    Move IsAncestor method to separate code challenge; clean up demo in Main

--- data-structures/BinaryTree/BinaryTree/Program.cs
+++ data-structures/BinaryTree/BinaryTree/Program.cs
@@ -8,68 +8,69 @@ namespace BinaryTree
 	{
 		static void Main(string[] args)
 		{
+			// Instantiate new node and add as root of new BT
+			Node<int> intNode = new Node<int>(11);
+			BinaryTree<int> tree = new BinaryTree<int>(intNode);
 
-			//// Instantiate new node and add as root of new BT
-			//Node<int> intNode = new Node<int>(11);
-			//BinaryTree<int> tree = new BinaryTree<int>(intNode);
+			// Add child nodes to BT
+			Node<int> left = new Node<int>(12);
+			Node<int> right = new Node<int>(13);
+			tree.Root.LeftChild = left;
+			tree.Root.RightChild = right;
 
-			//// Add child nodes to BT
-			//Node<int> left = new Node<int>(12);
-			//Node<int> right = new Node<int>(13);
-			//tree.Root.LeftChild = left;
-			//tree.Root.RightChild = right;
+			// Test PreOrder() method
+			List<int> outputOfPreOrder = tree.PreOrder(intNode);
 
-			//// Test PreOrder() method
-			//List<int> outputOfPreOrder = tree.PreOrder(intNode);
-			//Console.Write($"{outputOfPreOrder[0]} ");
-			//Console.Write($"{outputOfPreOrder[1]} ");
-			//Console.WriteLine($"{outputOfPreOrder[2]} ");
-			//Console.WriteLine("");
-			//tree.output.Clear();
+			foreach (int num in outputOfPreOrder)
+			{
+				Console.Write("{0} ", num);
+			}
+			Console.WriteLine("\n");
+			tree.output.Clear();
 
-			//// Test InOrder() method
-			//List<int> outputOfInOrder = tree.InOrder(intNode);
-			//Console.Write($"{outputOfInOrder[0]} ");
-			//Console.Write($"{outputOfInOrder[1]} ");
-			//Console.WriteLine($"{outputOfInOrder[2]} ");
-			//Console.WriteLine("");
-			//tree.output.Clear();
+			// Demo InOrder() method
+			List<int> outputOfInOrder = tree.InOrder(intNode);
+			foreach (int num in outputOfInOrder)
+			{
+				Console.Write("{0} ", num);
+			}
+			Console.WriteLine("\n");
+			tree.output.Clear();
 
-			//// Test PostOrder() method
-			//List<int> outputOfPostOrder = tree.PostOrder(intNode);
-			//Console.Write($"{outputOfPostOrder[0]} ");
-			//Console.Write($"{outputOfPostOrder[1]} ");
-			//Console.WriteLine($"{outputOfPostOrder[2]} ");
-			//Console.WriteLine("");
-			//tree.output.Clear();
+			// Demo PostOrder() method
+			List<int> outputOfPostOrder = tree.PostOrder(intNode);
+			foreach (int num in outputOfPostOrder)
+			{
+				Console.Write("{0} ", num);
+			}
+			Console.WriteLine("\n");
+			tree.output.Clear();
 
-			//// Test BST's Add() method
-			//Node<int> rootOfBST = new Node<int>(21);
-			//BinarySearchTree<int> bst = new BinarySearchTree<int>(rootOfBST);
-			//Console.WriteLine($"bst.Root.Value: {bst.Root.Value}");
+			// Demo BST's Add() method
+			Node<int> rootOfBST = new Node<int>(21);
+			BinarySearchTree<int> bst = new BinarySearchTree<int>(rootOfBST);
+			Console.WriteLine($"bst.Root.Value: {bst.Root.Value}");
 
-			//bst.Add(bst.Root, 22);
-			//Console.WriteLine($"bst.Root.RightChild.Value: {bst.Root.RightChild.Value}");
-			//Console.WriteLine("");
+			bst.Add(bst.Root, 22);
+			Console.WriteLine($"bst.Root.RightChild.Value: {bst.Root.RightChild.Value}");
+			Console.WriteLine("");
 
 			////////////////////////////////////////////
 			// Breadth-first binary tree traversal
 			////////////////////////////////////////////
 
 			Console.WriteLine("Node values from breadth-first binary tree traversal:");
-
-			BinaryTree<int> tree = new BinaryTree<int>();
-
-			Node<int> node1 = new Node<int>(11);
-			Node<int> node2 = new Node<int>(12);
-			Node<int> node3 = new Node<int>(13);
-			Node<int> node4 = new Node<int>(14);
-			Node<int> node5 = new Node<int>(15);
-			Node<int> node6 = new Node<int>(16);
-			Node<int> node7 = new Node<int>(17);
-			Node<int> node8 = new Node<int>(18);
-			Node<int> node9 = new Node<int>(19);
-			Node<int> node10 = new Node<int>(20);
+			
+			Node<int> node1 = new Node<int>(1);
+			Node<int> node2 = new Node<int>(2);
+			Node<int> node3 = new Node<int>(3);
+			Node<int> node4 = new Node<int>(4);
+			Node<int> node5 = new Node<int>(5);
+			Node<int> node6 = new Node<int>(6);
+			Node<int> node7 = new Node<int>(7);
+			Node<int> node8 = new Node<int>(8);
+			Node<int> node9 = new Node<int>(9);
+			Node<int> node10 = new Node<int>(10);
 
 			// Populate tree with nodes
 			tree.Root = node1;
@@ -85,67 +86,6 @@ namespace BinaryTree
 
 			// Traverse tree breadth-first
 			tree.TraverseBreadthFirst(tree);
-
-			////////////////////////////////////////////
-			// Demo IsAncestor() method
-			////////////////////////////////////////////
-
-			Console.WriteLine(IsAncestor(14, 19, tree.Root));
-			Console.WriteLine(IsAncestor(12, 20, tree.Root));
-			Console.WriteLine(IsAncestor(13, 20, tree.Root));
-			Console.WriteLine(IsAncestor(11, 77, tree.Root));
-		}
-
-
-		/// <summary>
-		/// Checks whether node with value a is ancestor of node with value b, treating both a and b as root nodes of their own trees in successive calls of recursive preorder method
-		/// </summary>
-		/// <param name="a">Value of ancestor node to check for</param>
-		/// <param name="b">Value of descendant node to check for</param>
-		/// <param name="root">Root note of tree</param>
-		/// <returns>True or false</returns>
-		public static bool? IsAncestor(int a, int b, Node<int> root)
-		{
-			if (root == null) return null; // Handle empty tree
-
-			bool flag = false;
-			Node<int> found = null;
-
-			// Internal method for PreOrder traversal
-			void PreOrderContains(int x, Node<int> node)
-			{
-				if (node.Value == x)
-				{
-					found = node;
-				}
-
-				if (node.LeftChild != null)
-				{
-					PreOrderContains(x, node.LeftChild);
-				}
-
-				if (node.RightChild != null)
-				{
-					PreOrderContains(x, node.RightChild);
-				}
-			}
-			
-			// Check whether tree contains first (ancestor) value
-			PreOrderContains(a, root);
-
-			if (found.Value == a)
-			{
-				// If tree contains ancestor value, check whether second (descendant) value exists downstream from ancestor
-				PreOrderContains(b, found);
-
-				// If so, set boolean flag to true
-				if (found.Value == b)
-				{
-					flag = true;
-				}
-			}
-
-			return flag;
 		}
 	}
 }
