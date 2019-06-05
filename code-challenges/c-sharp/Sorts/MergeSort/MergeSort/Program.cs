8b361ce56bf59aea1b43d192710a125a75b69696
Andrew Curtis <curtis.andrewm@gmail.com>
2019-06-05 14:19:09 -0700

    Add working Merge Sort solution

--- code-challenges/c-sharp/Sorts/MergeSort/MergeSort/Program.cs
+++ code-challenges/c-sharp/Sorts/MergeSort/MergeSort/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MergeSort
 {
@@ -16,15 +17,82 @@ namespace MergeSort
 			Console.WriteLine();
 
 			// Sort array
-			MergeSorter1(nums);
+			int[] sortedNums = MergeSort3(nums);
 
 			// Print array after sorting
-			for (int i = 0; i < nums.Length; i++)
+			for (int i = 0; i < sortedNums.Length; i++)
 			{
-				Console.Write("{0} ", nums[i]);
+				Console.Write("{0} ", sortedNums[i]);
 			}
 			Console.WriteLine();
 		}
+
+		public static int[] MergeSort3(int[] arr)
+		{
+			if (arr.Length <= 1) return arr;
+
+			int mid = arr.Length / 2;
+
+			int[] leftArr = new int[mid];
+			int[] rightArr = new int[arr.Length - mid];
+
+			Array.Copy(arr, 0, leftArr, 0, mid);
+			Array.Copy(arr, mid, rightArr, 0, arr.Length - mid);
+
+			return Merge(MergeSort3(leftArr), MergeSort3(rightArr), arr);
+		}
+
+		public static int[] Merge(int[] leftArr, int[] rightArr, int[] arr)
+		{
+			//int low = 0;
+			int mid = arr.Length / 2;
+			//int high = arr.Length - 1;
+
+			int leftIndex = 0;
+			int rightIndex = 0;
+			int currentIndex = 0;
+
+			//	int leftEnd = (leftStart + rightEnd) / 2;
+			//	int rightStart = leftEnd + 1;
+			//	int len = rightEnd - leftStart + 1;
+
+			//	int leftIndex = leftStart;
+			//	int rightIndex = rightStart;
+			//	int currentIndex = leftStart;
+
+			while (leftIndex < leftArr.Length && rightIndex < rightArr.Length)
+			{
+				if (leftArr[leftIndex] <= rightArr[rightIndex])
+				{
+					arr[currentIndex] = leftArr[leftIndex];
+					leftIndex++;
+				}
+				else
+				{
+					arr[currentIndex] = rightArr[rightIndex];
+					rightIndex++;
+				}
+				currentIndex++;
+			}
+
+			//int remaining = mid - leftIndex;
+			//for (int i = 0; i < remaining; i++)
+			//{
+			//	arr[currentIndex + i] = leftArr[leftIndex + i];
+			//}
+
+			if (leftIndex == leftArr.Length)
+			{
+				Array.Copy(rightArr, rightIndex, arr, currentIndex, rightArr.Length - rightIndex);
+			}
+			else
+			{
+				Array.Copy(leftArr, leftIndex, arr, currentIndex, leftArr.Length - leftIndex);
+			}
+
+			return arr;
+		}
+
 
 		// This algo for merge sort is taken straight from GLM's book to see if at least this one works
 		//public static void MergeSorter2(int[] array)
@@ -77,72 +145,73 @@ namespace MergeSort
 		//	}
 		//}
 
-		// Combined approach that borrows from both GLM's book and various online resources
-		public static void MergeSorter1(int[] arr)
-		{
-			int[] tempArr = new int[arr.Length];
-			MergeSort1(arr, tempArr, 0, arr.Length - 1);
-		}
 
-		public static void MergeSort1(int[] arr, int[] tempArr, int leftStart, int rightEnd)
-		{
-			if (leftStart < rightEnd)
-			{
-				int mid = (leftStart + rightEnd) / 2;
+		//// Combined approach that borrows from both GLM's book and various online resources
+		//public static void MergeSorter1(int[] arr)
+		//{
+		//	int[] tempArr = new int[arr.Length];
+		//	MergeSort1(arr, tempArr, 0, arr.Length - 1);
+		//}
 
-				MergeSort1(arr, tempArr, leftStart, mid);
-				MergeSort1(arr, tempArr, mid + 1, rightEnd);
-				Merge1(arr, tempArr, leftStart, rightEnd);
-			}
-		}
+		//public static void MergeSort1(int[] arr, int[] tempArr, int leftStart, int rightEnd)
+		//{
+		//	if (leftStart < rightEnd)
+		//	{
+		//		int mid = (leftStart + rightEnd) / 2;
 
-		public static void Merge1(int[] arr, int[] tempArr, int leftStart, int rightEnd)
-		{
-			int leftEnd = (leftStart + rightEnd) / 2;
-			int rightStart = leftEnd + 1;
-			int len = rightEnd - leftStart + 1;
+		//		MergeSort1(arr, tempArr, leftStart, mid);
+		//		MergeSort1(arr, tempArr, mid + 1, rightEnd);
+		//		Merge1(arr, tempArr, leftStart, rightEnd);
+		//	}
+		//}
 
-			int leftIndex = leftStart;
-			int rightIndex = rightStart;
-			int currentIndex = leftStart;
+		//public static void Merge1(int[] arr, int[] tempArr, int leftStart, int rightEnd)
+		//{
+		//	int leftEnd = (leftStart + rightEnd) / 2;
+		//	int rightStart = leftEnd + 1;
+		//	int len = rightEnd - leftStart + 1;
 
-			while (leftIndex <= leftEnd && rightIndex <= rightEnd)
-			{
-				if (arr[leftIndex] <= arr[rightIndex])
-				{
-					arr[leftIndex] = tempArr[currentIndex];
-					leftIndex++;
-				}
-				else
-				{
-					arr[rightIndex] = tempArr[currentIndex];
-					rightIndex++;
-				}
-				currentIndex++;
-			}
+		//	int leftIndex = leftStart;
+		//	int rightIndex = rightStart;
+		//	int currentIndex = leftStart;
 
-			// Copy over rest of left subarray into original array
-			// Attempt #1
-			int remaining = leftEnd - leftIndex;
-			for (int i = 0; i < remaining; i++)
-			{
-				arr[currentIndex + i] = tempArr[leftIndex];
-			}
+		//	while (leftIndex <= leftEnd && rightIndex <= rightEnd)
+		//	{
+		//		if (arr[leftIndex] <= arr[rightIndex])
+		//		{
+		//			arr[leftIndex] = tempArr[currentIndex];
+		//			leftIndex++;
+		//		}
+		//		else
+		//		{
+		//			arr[rightIndex] = tempArr[currentIndex];
+		//			rightIndex++;
+		//		}
+		//		currentIndex++;
+		//	}
 
-			// Attempt #2
-			//Array.Copy(arr, leftIndex, tempArr, currentIndex, leftEnd - leftIndex + 1);
-			//Array.Copy(arr, rightIndex, tempArr, currentIndex, rightEnd - rightIndex + 1);
-			//Array.Copy(tempArr, leftStart, arr, leftStart, len);
+		//	// Copy over rest of left subarray into original array
+		//	// Attempt #1
+		//	int remaining = leftEnd - leftIndex;
+		//	for (int i = 0; i < remaining; i++)
+		//	{
+		//		arr[currentIndex + i] = tempArr[leftIndex];
+		//	}
 
-			//// Attempt #3
-			//if (leftIndex == tempArr.Length)
-			//{
-			//	Array.Copy(arr, rightIndex, tempArr, currentIndex, rightIndex - rightIndex + 1);
-			//}
-			//else
-			//{
-			//	Array.Copy(arr, leftIndex, tempArr, currentIndex, leftEnd - leftIndex + 1);
-			//}
-		}
+		// Attempt #2
+		//Array.Copy(arr, leftIndex, tempArr, currentIndex, leftEnd - leftIndex + 1);
+		//Array.Copy(arr, rightIndex, tempArr, currentIndex, rightEnd - rightIndex + 1);
+		//Array.Copy(tempArr, leftStart, arr, leftStart, len);
+
+		//// Attempt #3
+		//if (leftIndex == tempArr.Length)
+		//{
+		//	Array.Copy(arr, rightIndex, tempArr, currentIndex, rightEnd - rightIndex + 1);
+		//}
+		//else
+		//{
+		//	Array.Copy(arr, leftIndex, tempArr, currentIndex, leftEnd - leftIndex + 1);
+		//}
+		//}
 	}
 }
