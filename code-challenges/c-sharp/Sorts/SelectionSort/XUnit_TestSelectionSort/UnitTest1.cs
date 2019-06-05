ab30d84822d75bd51de433eff13469b0520be68f
Andrew Curtis <curtis.andrewm@gmail.com>
2019-06-04 19:24:26 -0700

    Add passing unit tests for Selection Sort

--- code-challenges/c-sharp/Sorts/SelectionSort/XUnit_TestSelectionSort/UnitTest1.cs
+++ code-challenges/c-sharp/Sorts/SelectionSort/XUnit_TestSelectionSort/UnitTest1.cs
@@ -1,3 +1,4 @@
+using SelectionSort;
 using System;
 using Xunit;
 
@@ -8,37 +9,79 @@ namespace XUnit_TestSelectionSort
 		[Fact]
 		public void CanSortUnsortedArray()
 		{
+			// Arrange
+			int[] nums = new int[] { 7, 4, 3, 9, 8, 6, 2, 1, 5 };
 
+			// Act
+			Program.SelectionSort(nums);
+
+			// Assert
+			Assert.Equal(9, nums[nums.Length - 1]);
 		}
 
 		[Fact]
 		public void CanReturnAlreadySortedArray()
 		{
+			// Arrange
+			int[] nums = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
+			// Act
+			Program.SelectionSort(nums);
+
+			// Assert
+			Assert.Equal(9, nums[nums.Length - 1]);
 		}
 
 		[Fact]
 		public void CanSortBackwardSortedArray()
 		{
+			// Arrange
+			int[] nums = new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
 
+			// Act
+			Program.SelectionSort(nums);
+
+			// Assert
+			Assert.Equal(9, nums[nums.Length - 1]);
 		}
 
 		[Fact]
 		public void CanHandleEmptyArray()
 		{
+			// Arrange
+			int[] nums = new int[0];
 
+			// Act
+			Program.SelectionSort(nums);
+
+			// Assert
+			Assert.Empty(nums);
 		}
 
 		[Fact]
 		public void CanSortArrayOfOneElement()
 		{
+			// Arrange
+			int[] nums = new int[] { 9 };
 
+			// Act
+			Program.SelectionSort(nums);
+
+			// Assert
+			Assert.Equal(9, nums[nums.Length - 1]);
 		}
 
 		[Fact]
 		public void CanSortArrayWithDuplicateValues()
 		{
+			// Arrange
+			int[] nums = new int[] { 7, 4, 3, 9, 9, 8, 6, 2, 1, 5 };
 
+			// Act
+			Program.SelectionSort(nums);
+
+			// Assert
+			Assert.Equal(9, nums[nums.Length - 2]);
 		}
 	}
 }
