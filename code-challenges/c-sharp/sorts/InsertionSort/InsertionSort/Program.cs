aa76012014f654bf21b79ef899980d0283914cda
Andrew Curtis <curtis.andrewm@gmail.com>
2019-06-05 20:35:50 -0700

    Delete and/or rename sorts and array-binary-search dirs to try to fix git remote mixup

---
+++ code-challenges/c-sharp/sorts/InsertionSort/InsertionSort/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace InsertionSort
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] nums = new int[] { 4, 3, 1, 9, 7, 5, 8, 6, 2 };
+
+			// Print array before sorting
+			for (int i = 0; i < nums.Length; i++)
+			{
+				Console.Write("{0} ", nums[i]);
+			}
+			Console.WriteLine();
+
+			// Sort array
+			InsertionSort(nums);
+
+			// Print array after sorting
+			for (int i = 0; i < nums.Length; i++)
+			{
+				Console.Write("{0} ", nums[i]);
+			}
+			Console.WriteLine();
+		}
+
+		/// <summary>
+		/// Sorts array in place using insertion sort with O(n^2) time complexity
+		/// </summary>
+		/// <param name="arr">array to sort</param>
+		public static void InsertionSort(int[] arr)
+		{
+			int j, temp;
+
+			for (int i = 1; i < arr.Length; i++)
+			{
+				j = i - 1;
+				temp = arr[i];
+
+				while (j > -1 && temp < arr[j])
+				{
+					arr[j + 1] = arr[j];
+					j = j - 1;
+				}
+
+				arr[j + 1] = temp;
+			}
+		}
+	}
+}
---
+++ code-challenges/c-sharp/sorts/SelectionSort/SelectionSort/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace SelectionSort
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] nums = new int[] { 7, 4, 3, 9, 8, 6, 2, 10, 5 };
+
+			// Print array before sorting
+			foreach (int num in nums)
+			{
+				Console.Write("{0} ", num);
+			}
+			Console.WriteLine();
+
+			// Sort array
+			SelectionSort(nums);
+
+			// Print array after sorting
+			foreach (int num in nums)
+			{
+				Console.Write("{0} ", num);
+			}
+			Console.WriteLine();
+		}
+
+		/// <summary>
+		/// Sorts array in place using selection sort with O(n^2) time complexity
+		/// </summary>
+		/// <param name="arr">array to sort</param>
+		public static void SelectionSort(int[] arr)
+		{
+			int min, temp;
+
+			for (int i = 0; i < arr.Length - 1; i++)
+			{
+				min = i; // min points to index of current lowest num
+
+				for (int j = i + 1; j < arr.Length; j++)
+				{
+					if (arr[j] < arr[min])
+					{
+						min = j;
+					}
+				}
+				temp = arr[i];
+				arr[i] = arr[min];
+				arr[min] = temp;
+			}
+		}
+	}
+}
