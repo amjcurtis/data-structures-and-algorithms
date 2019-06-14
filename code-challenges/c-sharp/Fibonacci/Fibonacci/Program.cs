65a923a77045a23a21b30d55e24592bdbb989163
Andrew Curtis <curtis.andrewm@gmail.com>
2019-06-14 10:39:06 -0700

    Add working recursive Fibonacci solution

---
+++ code-challenges/c-sharp/Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Fibonacci
+{
+	class Program
+	{
+		private static int fibCount = 0;
+
+		static void Main(string[] args)
+		{
+			// 0 1 1 2 3 5 8 13 21 34 55
+			int fib = FibonacciRecursive(3);
+			Console.WriteLine();
+			Console.WriteLine(fib);
+			Console.WriteLine(fibCount);
+		}
+		
+		public static int FibonacciRecursive(int n)
+		{
+			fibCount++;
+			Console.WriteLine(n);
+			if (n < 2) return n; // Base case
+			return FibonacciRecursive(n - 2) + FibonacciRecursive(n - 1);
+		}
+	}
+}
