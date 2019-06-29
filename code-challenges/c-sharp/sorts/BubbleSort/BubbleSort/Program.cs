eec3b1ca7d4fe923abac351f7dfb57ad4ae4cb7d
Andrew Curtis <curtis.andrewm@gmail.com>
2019-06-29 00:35:42 -0700

    Optimize bubble sort to improve time efficiency

--- code-challenges/c-sharp/sorts/BubbleSort/BubbleSort/Program.cs
+++ code-challenges/c-sharp/sorts/BubbleSort/BubbleSort/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace BubbleSort
 {
@@ -6,7 +7,18 @@ namespace BubbleSort
 	{
 		static void Main(string[] args)
 		{
-			int[] nums = new int[] { 4, 3, 1, 9, 7, 5, 8, 6, 2 };
+			//int[] nums = new int[] { 4, 3, 1, 9, 7, 5, 8, 6, 2 };
+
+			// Generate array of random numbers
+			int size = 2500;
+			int[] nums = new int[size];
+			Random random = new Random();
+			int randomRange = size * 4;
+			for (int i = 0; i < size; i++)
+			{
+				int rand = random.Next(1, randomRange);
+				nums[i] = rand;
+			}
 
 			// Print array before sorting
 			for (int i = 0; i < nums.Length; i++)
@@ -16,7 +28,9 @@ namespace BubbleSort
 			Console.WriteLine();
 
 			// Sort array
+			Stopwatch stopwatch = Stopwatch.StartNew();
 			BubbleSort(nums);
+			stopwatch.Stop();
 
 			// Print array after sorting
 			for (int i = 0; i < nums.Length; i++)
@@ -24,6 +38,7 @@ namespace BubbleSort
 				Console.Write("{0} ", nums[i]);
 			}
 			Console.WriteLine();
+			Console.WriteLine(stopwatch.Elapsed);
 		}
 
 		/// <summary>
@@ -34,15 +49,15 @@ namespace BubbleSort
 		{
 			int temp;
 
-			for (int i = 0; i < arr.Length - 1; i++)
+			for (int i = 0; i < arr.Length; i++)
 			{
-				for (int j = 1; j < arr.Length; j++)
+				for (int j = 0; j < arr.Length - i - 1; j++)
 				{
-					if (arr[j] < arr[j - 1])
+					if (arr[j] > arr[j + 1])
 					{
 						temp = arr[j];
-						arr[j] = arr[j - 1];
-						arr[j - 1] = temp;
+						arr[j] = arr[j + 1];
+						arr[j + 1] = temp;
 					}
 				}
 			}
