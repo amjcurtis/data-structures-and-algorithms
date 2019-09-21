20e2296c4e2126e7b8c1ebefd47604d4295d3ce7
Andrew Curtis <Andrew_Curtis@selinc.com>
2019-09-20 17:53:25 -0700

    Add unit test for myString.reverse method

--- code-challenges/golang/main_test.go
+++ code-challenges/golang/main_test.go
@@ -5,8 +5,15 @@ import "testing"
 func TestReverseString(t *testing.T) {
 	str := "word"
 	got := reverseString(str)
-	want := "drow"
-	if got != want {
+	if want := "drow"; got != want {
 		t.Errorf("reverseString(%s) = %s; want %s", str, got, want)
+	}
+}
+
+func TestReverse(t *testing.T) {
+	myStr := myString("text")
+	got := myStr.reverse()
+	if want := myString("txet"); got != want {
+		t.Errorf("%s.reverse() = %s; want %s", myStr, got, want)
 	}
 }
